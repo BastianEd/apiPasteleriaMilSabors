83d956b95e76ea220b35b188cf992c7a822444f5
BastianEd <ba.rubiom@duocuc.cl>
2025-12-12 12:57:05 -0300

    Eliminar comentarios innecesarios en el módulo de publicaciones para mejorar la legibilidad del código

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'; // <--- Importante
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { Post } from './entities/post.entity'; // <--- Importar la Entidad
+import { Post } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])], // <--- Registrar la Entidad aquí
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
 })
